55cc8ed99a571dfbd5063f4151d951b4482cb702
li.bo <bo.li@synsense.ai>
2023-06-14 15:13:51 +0800

    lenet

---
+++ Pytorch_classification/Letnet/train.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+@Author  : Morvan Li
+@FileName: train.py
+@Software: PyCharm
+@Time    : 6/14/23 2:31 PM
+"""
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from model import LeNet
+
+
+def main():
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+
+    # 50000张训练图片
+    # 第一次使用时要将download设置为True才会自动去下载数据集
+    train_set = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                             download=True, transform=transform)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=36,
+                                               shuffle=True, num_workers=4)
+
+    # 10000张验证图片
+    # 第一次使用时要将download设置为True才会自动去下载数据集
+    val_set = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    val_loader = torch.utils.data.DataLoader(val_set, batch_size=5000,
+                                             shuffle=False, num_workers=0)
+    val_data_iter = iter(val_loader)
+    val_image, val_label = next(val_data_iter)
+
+    net = LeNet()
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+    for epoch in range(100):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for step, data in enumerate(train_loader, start=0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = loss_function(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if step % 500 == 499:  # print every 500 mini-batches
+                with torch.no_grad():
+                    outputs = net(val_image)  # [batch, 10]
+                    predict_y = torch.max(outputs, dim=1)[1]
+                    accuracy = torch.eq(predict_y, val_label).sum().item() / val_label.size(0)
+
+                    print('[%d, %5d] train_loss: %.3f  test_accuracy: %.3f' %
+                          (epoch + 1, step + 1, running_loss / 500, accuracy))
+                    running_loss = 0.0
+
+    print('Finished Training')
+
+    save_path = './Lenet.pth'
+    torch.save(net.state_dict(), save_path)
+
+
+if __name__ == '__main__':
+    main()
