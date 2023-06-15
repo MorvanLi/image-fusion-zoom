72dc87b6939feceeb11afb378b2f297d94df3721
li.bo <bo.li@synsense.ai>
2023-06-15 14:33:54 +0800

    vgg

---
+++ Pytorch_classification/Vgg/model.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+@Author  : Morvan Li
+@FileName: model.py
+@Software: PyCharm
+@Time    : 6/15/23 10:33 AM
+"""
+
+import torch
+import torch.nn as nn
+
+# official pretrain weights
+model_urls = {
+    'vgg11': 'https://download.pytorch.org/models/vgg11-bbd30ac9.pth',
+    'vgg13': 'https://download.pytorch.org/models/vgg13-c768596a.pth',
+    'vgg16': 'https://download.pytorch.org/models/vgg16-397923af.pth',
+    'vgg19': 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth'
+}
+
+
+class VGG(nn.Module):
+    def __init__(self, features, num_classes=1000, init_weights=False):
+        super(VGG, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, num_classes)
+        )
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        # N x 3 x 224 x 224
+        x = self.features(x)
+        # N x 512 x 7 x 7
+        x = torch.flatten(x, start_dim=1)
+        # N x 512*7*7
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                # nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                # nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+
+def make_features(cfg: list):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == "M":
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            layers += [conv2d, nn.ReLU(True)]
+            in_channels = v
+            '''
+            *layer是将列表中的层对象拆解,并将它们作为独立的参数传递给nn.Sequential函数。
+            nn.Sequential函数期望接收多个参数，每个参数都是一个层对象，因此使用*操作符可以将layers列表中的层对象解包为多个参数。
+            如果不使用*操作符，那么nn.Sequential(layers)将会把整个layers列表作为一个参数传递给nn.Sequential，
+            这将导致错误。因为nn.Sequential期望接收多个参数，而不是一个包含层对象的列表。
+            '''
+    return nn.Sequential(*layers)
+
+
+cfgs = {
+    'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'vgg19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+def vgg(model_name="vgg16", **kwargs):
+    assert model_name in cfgs, "Warning: model number {} not in cfgs dict!".format(model_name)
+    cfg = cfgs[model_name]
+    model = VGG(make_features(cfg), **kwargs)
+    return model
+
+
+if __name__ == '__main__':
+    model = vgg()
+    print(model)
---
+++ Pytorch_classification/Vgg/train.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+@Author  : Morvan Li
+@FileName: train.py
+@Software: PyCharm
+@Time    : 6/15/23 11:01 AM
+"""
+
+import json
+import os
+import sys
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from model import vgg
+from torchvision import datasets, transforms
+from tqdm import tqdm
+import torchvision.models as models
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("using {} device.".format(device))
+
+    data_transform = {
+        "train": transforms.Compose([transforms.RandomResizedCrop(224),
+                                     transforms.RandomHorizontalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),
+        "val": transforms.Compose([transforms.Resize((224, 224)),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])}
+
+    data_root = os.path.abspath(os.path.join(os.getcwd(), ".."))  # get data root path
+    image_path = os.path.join(data_root, "data_set", "flower_data")  # flower data set path
+    assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
+    train_dataset = datasets.ImageFolder(root=os.path.join(image_path, "train"),
+                                         transform=data_transform["train"])
+    train_num = len(train_dataset)
+
+    # {'daisy':0, 'dandelion':1, 'roses':2, 'sunflower':3, 'tulips':4}
+    flower_list = train_dataset.class_to_idx
+    cla_dict = dict((val, key) for key, val in flower_list.items())
+    # write dict into json file
+    json_str = json.dumps(cla_dict, indent=4)
+    with open('class_indices.json', 'w') as json_file:
+        json_file.write(json_str)
+
+    batch_size = 8
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    print('Using {} dataloader workers every process'.format(nw))
+
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size, shuffle=True,
+                                               num_workers=nw)
+
+    validate_dataset = datasets.ImageFolder(root=os.path.join(image_path, "val"),
+                                            transform=data_transform["val"])
+    val_num = len(validate_dataset)
+    validate_loader = torch.utils.data.DataLoader(validate_dataset,
+                                                  batch_size=batch_size, shuffle=False,
+                                                  num_workers=nw)
+    print("using {} images for training, {} images for validation.".format(train_num,
+                                                                           val_num))
+
+    # test_data_iter = iter(validate_loader)
+    # test_image, test_label = test_data_iter.next()
+    # 方法一: 使用自己定义的vgg模型
+    # model_name = "vgg16"
+    # net = vgg(model_name=model_name, num_classes=5, init_weights=True)
+    # net.to(device)
+
+    #　方法二：使用预训练模型VGG, 5个epoch就能达到83%的准确率
+    net = models.vgg16(pretrained=True)
+    for param in net.parameters():
+        param.requires_grad = False
+    num_feature = net.classifier[-1].in_features
+    net.classifier[-1] = torch.nn.Linear(num_feature, 5)
+    net = net.to(device)
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.0001)
+
+    epochs = 5
+    best_acc = 0.0
+    save_path = './vgg16Net.pth'
+    train_steps = len(train_loader)
+    for epoch in range(epochs):
+        # train
+        net.train()
+        running_loss = 0.0
+        train_bar = tqdm(train_loader, file=sys.stdout)
+        for step, data in enumerate(train_bar):
+            images, labels = data
+            optimizer.zero_grad()
+            outputs = net(images.to(device))
+            loss = loss_function(outputs, labels.to(device))
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+
+            train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,
+                                                                     epochs,
+                                                                     loss)
+
+        # validate
+        net.eval()
+        acc = 0.0  # accumulate accurate number / epoch
+        with torch.no_grad():
+            val_bar = tqdm(validate_loader, file=sys.stdout)
+            for val_data in val_bar:
+                val_images, val_labels = val_data
+                outputs = net(val_images.to(device))
+                predict_y = torch.max(outputs, dim=1)[1]
+                acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
+
+        val_accurate = acc / val_num
+        print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %
+              (epoch + 1, running_loss / train_steps, val_accurate))
+
+        if val_accurate > best_acc:
+            best_acc = val_accurate
+            torch.save(net.state_dict(), save_path)
+
+    print('Finished Training')
+
+
+if __name__ == '__main__':
+    main()
